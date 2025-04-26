9ef7aef549c0791b93f08ce3ad03b18e66fcc162
mohammad hossein <sheikhmohem@gmail.com>
2025-04-26 17:19:56 +0330

    added : session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import JotaiProvider from "@/components/jotaiProvider";
+import SessionWrapper from "@/components/nextauthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <JotaiProvider>{children}</JotaiProvider>
+        <SessionWrapper>
+          <JotaiProvider>{children}</JotaiProvider>
+        </SessionWrapper>
       </body>
     </html>
   );
